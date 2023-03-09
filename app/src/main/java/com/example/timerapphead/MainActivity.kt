d16cb7d539ea99f4d2452a8fc053c754105907b5
nurik17 <91252221+nurik17@users.noreply.github.com>
2023-03-09 12:17:10 +0600

    timer app

---
+++ app/src/main/java/com/example/timerapphead/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.timerapphead
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Chronometer
+
+class MainActivity : AppCompatActivity() {
+    lateinit var stopwatch : Chronometer
+    var running = false
+    var offset :Long = 0
+
+    //
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        stopwatch = findViewById<Chronometer>(R.id.stopwatch)
+
+        if(savedInstanceState!=null){
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if(running){
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            }else setBaseTime()
+        }
+
+
+        val startButton = findViewById<Button>(R.id.start_button)
+        startButton.setOnClickListener {
+            if(!running){
+                setBaseTime()
+                stopwatch.start()
+                running = true
+            }
+        }
+        val pauseButton = findViewById<Button>(R.id.pause_button)
+        pauseButton.setOnClickListener {
+            saveOffSet()
+            stopwatch.stop()
+            running = false
+        }
+        val resetButton = findViewById<Button>(R.id.reset_button)
+        resetButton.setOnClickListener {
+            offset = 0
+            setBaseTime()
+        }
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY,offset)
+        savedInstanceState.putBoolean(RUNNING_KEY,running)
+        savedInstanceState.putLong(BASE_KEY,stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    private fun setBaseTime() {
+        stopwatch.base = SystemClock.elapsedRealtime()-offset
+    }
+
+    fun saveOffSet() {
+        offset = SystemClock.elapsedRealtime() - stopwatch.base
+    }
+
+}
